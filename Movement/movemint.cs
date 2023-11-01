b2c5564c027e3189f7d5cea66235255ba118846e
SHIRAYAKA\aqoft <shirayaka@gmail.com>
2023-11-01 20:43:48 +0700

    added basic Gravity

--- Movement/movemint.cs
+++ Movement/movemint.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class movemint : MonoBehaviour
@@ -7,9 +8,14 @@ public class movemint : MonoBehaviour
     
     public CharacterController controller;
 
-    Vector3 velocity;
     public float speed = 12f;
     public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
     void Start()
     {
         
@@ -18,6 +24,13 @@ public class movemint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
